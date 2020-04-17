edf0e798cfb9506a3782d6a934289f082e0c231f
Alex Volkov <alexvolkov72305@yandex.by>
2020-04-17 14:44:08 +0300

    14:40 17.04.2020 add site v 0.0.2

--- src/assets/scripts/works.js
+++ src/assets/scripts/works.js
@@ -2,9 +2,9 @@ import script from "./modules/script"
 import bootstrap from "bootstrap"
 import FontAwesomeFree from "@fortawesome/fontawesome-free/js/all"
 /*import MagnificPopup from "magnific-popup/dist/jquery.magnific-popup.min"*/
-
 import slick from "slick-carousel/slick/slick"
 import jqueryScript from "./modules/jqueryScript"
+
 
 script();
 bootstrap();
@@ -12,3 +12,5 @@ FontAwesomeFree();
 slick();
 /*MagnificPopup();*/
 jqueryScript();
+
+
